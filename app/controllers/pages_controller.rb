948110debb0cce109e7c68877cbc8d48a5d6536b
Matt Smith <matt@nearapogee.com>
2013-08-29 14:10:28 -0700

    add more homepage work.

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -3,6 +3,8 @@ class PagesController < ApplicationController
   def home
     @first_movie = Movie.first
     @first_votes = @first_movie.votes.count
+
+    @my_favorite = Movie.find_by title: "Without a Paddle" # Change to your favorite
   end
 
 end
