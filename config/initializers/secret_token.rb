bafb4696c44601146877a1a71bdb1ba89d549873
Matt Smith <matt@nearapogee.com>
2013-08-29 13:25:20 -0700

    inital commit.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+CfMovies::Application.config.secret_key_base = 'e70d9bfecd011fa091c2edb99ea65877670d3ac48b1c3966c207a792a9be8be19464348aecb5accd145f485c09ede7e195b2aceb4c8ec68de8d346442bdf1cdf'
