fa53500532356cb4a70cbf7596d3a8199e23ed2e
Matt Smith <matt@nearapogee.com>
2013-08-29 14:17:37 -0700

    add movies

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,8 @@ CfMovies::Application.routes.draw do
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
+  resources :movies
+
   # You can have the root of your site routed with "root"
   # root 'welcome#index'
   root 'pages#home'
