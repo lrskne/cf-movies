74bbcc141f9106575ae3024370d316582ed15b71
Laura Erskine <lrskne@gmail.com>
2013-09-05 12:46:01 -0700

    less data transfer to home.html.erb view

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,10 +1,9 @@
 class PagesController < ApplicationController
 
   def home
-  #@movie = Movie.first.title
-  #Movie.first.votes.count
   @movie = Movie.first
-
+  @movie_title = @movie.title
+  @movie_votes_count = @movie.votes.count
 
   end
 
